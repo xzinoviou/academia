d8eb7cba01d825f94e9c86c3b4e0877d7f834c52
xzinoviou <xzinoviou@gmail.com>
2021-03-14 23:53:14 +0200

    uploaded initial microservices

---
+++ eureka-server/src/test/java/com/xzinoviou/academia/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.academia.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ result-service/src/test/java/com/xzinoviou/academia/resultservice/ResultServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.academia.resultservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
